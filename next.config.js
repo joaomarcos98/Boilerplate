2bd83179bc6461f482dbe67b4c2608397fdc7bef
Joao Marcos <joomarcos.belanga98@gmail.com>
2022-03-06 00:12:51 -0300

    new: adding pwa in project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPwa = require("next-pwa");
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+module.exports = withPwa({
+    pwa: {
+        dest: "public",
+        disable: !isProd
+    }
+});
